95da3299ef6c2ba196c8ec8e7bcf26daf05493b4
PRAVEENKUMAR R <145641656+praveenatinfotech@users.noreply.github.com>
2026-06-26 02:09:10 +0530

    Added Module 2

---
+++ Module 2-Data structures and Algorithms/E-commerce Platform Search Function/src/SearchAlgorithms.java
@@ -0,0 +1,45 @@
+public class SearchAlgorithms {
+
+    // Linear Search
+    public static Product linearSearch(Product[] products, String targetName) {
+
+        for (Product product : products) {
+
+            if (product.getProductName()
+                    .equalsIgnoreCase(targetName)) {
+
+                return product;
+            }
+        }
+
+        return null;
+    }
+
+    // Binary Search
+    public static Product binarySearch(Product[] products, String targetName) {
+
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int comparison =
+                    products[mid].getProductName()
+                            .compareToIgnoreCase(targetName);
+
+            if (comparison == 0) {
+                return products[mid];
+            }
+
+            if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return null;
+    }
+}
