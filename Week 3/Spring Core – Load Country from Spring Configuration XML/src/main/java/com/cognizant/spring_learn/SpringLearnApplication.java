1dada49bce662238944f7d9a6ad52ff10dd7fea4
PRAVEENKUMAR R <145641656+praveenatinfotech@users.noreply.github.com>
2026-06-30 22:12:31 +0530

    Updated Week 3

---
+++ Week 3/Spring Core – Load Country from Spring Configuration XML/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,22 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		displayCountry();
+	}
+
+	public static void displayCountry() {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country =
+				context.getBean("country", Country.class);
+
+		System.out.println(country);
+	}
+}
