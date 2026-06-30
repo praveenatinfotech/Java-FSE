0de43e9f4edcfdf2d72624da5ad7bfb478f0bf56
PRAVEENKUMAR R <145641656+praveenatinfotech@users.noreply.github.com>
2026-06-30 21:27:54 +0530

    Hands-on Exercise -- Week 2

---
+++ Week 2/Spring Core and Maven/Creating and Configuring a Maven Project/LibraryManagement/src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                (BookService) context.getBean("bookService");
+
+        BookRepository repository =
+                (BookRepository) context.getBean("bookRepository");
+
+        service.displayService();
+        repository.displayRepository();
+    }
+}
